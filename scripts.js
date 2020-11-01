f88e0c4faaf9f05f4d118e82cace8e3f4f58bd07
Furuta-sama <tday360@gmail.com>
2020-11-01 16:05:24 -0500

    Finalizing changes and JavaScript addition

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+
+window.onload = function() {
+    buttons = document.getElementsByClassName("btn");
+    formButton = buttons[1];
+    formTextField = document.getElementById("email")
+    message = document.getElementsByClassName("message")[0]
+
+    formButton.addEventListener("click", function(){
+        if (formTextField.value == ""){
+            message.innerHTML = "Please enter a valid email address";
+        }
+        else{
+            message.innerHTML = "Thank you! Your email address " + formTextField.value + "has been added to our mailing list!"
+        }
+    })
+}
